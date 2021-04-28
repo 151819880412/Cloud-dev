509b719f8842358ca65c1f2062195cee23582f09
151819880412 <1518198804@qq.com>
2021-04-28 13:10:24 +0800

    SDK

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Cloudbase from '@cloudbase/vue-provider'
+Vue.use(Cloudbase, {
+  env: 'hello-cloudbase-4gg7bj4836d2d514'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
